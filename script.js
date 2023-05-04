fd45289a92c87f7def0bf6d9759a61f2fbbe7f2e
Irina <ira.verbitskaia@gmail.com>
2023-05-04 17:24:20 +0300

    remote

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 'use strict'
-const result = prompt('What is your name?' , '');
-alert(`hello,${result}`);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},    
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?' , '');
+const lastFilmValue = prompt('На сколько оцените его?' , '');
+const lastFilm2 = prompt('Один из последних просмотренных фильмов?' , '');
+const lastFilmValue2 = prompt('На сколько оцените его?' , '');
+
+ personalMovieDB.movies[lastFilm] = lastFilmValue;
+ personalMovieDB.movies[lastFilm2] = lastFilmValue2;
+ 
+ console.log(personalMovieDB);
+
