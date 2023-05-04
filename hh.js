8b7fe3e2a6effb66e5fc5fe8c277581b9d924ebe
Irina <ira.verbitskaia@gmail.com>
2023-05-04 18:08:16 +0300

    switch

---
+++ hh.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log("no");
+        break;
+    case 50:
+        console.log('yes');
+        break;
+    default:
+        console.log('not tooday');
+        break;
+}
