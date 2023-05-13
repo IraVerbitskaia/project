e5878c0062738f7638dfb4eb9dfce49fb0c36ac5
Irina <ira.verbitskaia@gmail.com>
2023-05-14 01:37:07 +0300

    in the night

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 'use strict';
-let numberOfFilms;
-
-function start() {
-   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-       numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-   }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},    
     actors: {},
     genres: [],
-    privat: false
-};
-
-
- function rememberMyFilms () {
+    privat: false,
+    start: function() {
+      personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+   
+      while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+         personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+      }
+   },
+    rememberMyFilms: function() {
    for ( let i = 0; i <2; i++) { 
       let a = prompt('Один из последних просмотренных фильмов?' , ''),
           b = prompt('На сколько оцените его?' , '');
@@ -34,10 +27,9 @@ const personalMovieDB = {
               i--;
             }
        }  
- }
- rememberMyFilms();
+ },
 
- function detectPersonalLevel() {
+    detectPersonalLevel: function() {
    if (personalMovieDB.count < 10) {
       alert('Просмотрено довольно мало фильмов');
    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30) {
@@ -47,23 +39,29 @@ const personalMovieDB = {
    } else {
       alert("Произошла ошибка")
    }
- }
- detectPersonalLevel();
-
- function showMyDB (hidden) {
+ },
+    showMyDB: function(hidden) {
    if (!hidden) {
      console.log(personalMovieDB);
    }
-  }
-  
-  showMyDB(personalMovieDB.privat);
-
-
- function writeYourGenres () {
+  },
+    writeYourGenres: function() {
    for (let i = 0; i < 3; i++) {
+      if (personalMovieDB.genres == "" || personalMovieDB.genres == null) {
+         i--;
+      }
       personalMovieDB.genres[i+1] = prompt(`Ваш любимый жанр под номером ${i+1}`);
    }
- }
-
-writeYourGenres();
+  personalMovieDB.genres.forEach((item, i) => {
+   console.log (`Любимый жанр #${i+1} -это ${item}`);
+  });
+ },
+    togleVisibleMyDB: function() {
+   if (personalMovieDB.privat) {
+      personalMovieDB.privat = falsee;
+   } else {
+      personalMovieDB.privat = true;
+   }
+ },
  
+};
